7403e95ba1828581afa7b981a05c0000a0692c26
Sorokovsky <Sorokovskys@ukr.net>
2025-04-04 21:41:17 +0300

    refactor

--- src/main/java/org/sorokovsky/jwtauth/resolver/CurrentUserResolver.java
+++ src/main/java/org/sorokovsky/jwtauth/resolver/CurrentUserResolver.java
@@ -2,9 +2,10 @@ package org.sorokovsky.jwtauth.resolver;
 
 import lombok.RequiredArgsConstructor;
 import org.sorokovsky.jwtauth.annotation.CurrentUser;
+import org.sorokovsky.jwtauth.contract.Token;
+import org.sorokovsky.jwtauth.repository.UsersRepository;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -14,7 +15,7 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @Component
 @RequiredArgsConstructor
 public class CurrentUserResolver implements HandlerMethodArgumentResolver {
-    private final UserDetailsService userDetailsService;
+    private final UsersRepository repository;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -24,7 +25,11 @@ public class CurrentUserResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         final var authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated()) return null;
-        return userDetailsService.loadUserByUsername(authentication.getName());
+        if (authentication == null) return null;
+        if (authentication.getPrincipal() instanceof Token token) {
+            final var user = repository.findByEmail(token.email()).orElse(null);
+            return user;
+        }
+        return null;
     }
 }
